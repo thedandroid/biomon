a666df20046170fd68aaebac5ec84d10bfd03d62
Daniel Allen <daniel.l.allen@gmail.com>
2026-01-22 15:36:03 -0700

    Remove test file

--- test-hook.js
+++
@@ -1,9 +0,0 @@
-// Test file with intentional linting errors
-const foo = "bar";   // missing semicolon
-const unused = 123; // unused variable
-
-function badIndent() {
-  console.log("bad indentation");
-}
-
-module.exports = { foo };
